ad2cfb7da7cdcc48f6f825f667efe1502f2ecd7e
Joshua <lunalijoshua1@gmail.com>
2025-10-28 16:52:55 -0400

    Linear Regression Prediction

--- RegressionAssignment.py
+++ RegressionAssignment.py
@@ -43,14 +43,14 @@ plt.plot(X, y_pred, 'r') # 5 points
 fig.canvas.draw()
 plt.show()
 
-# # # Complete the following print statement (replace the blanks _____ by using a command, do not hard-code the values):
-# print("The linear relationship between X and y was modeled according to the equation: y = b_0 + X*b_1, \
-# where the bias parameter b_0 is equal to ", reg.intercept_, " and the weight b_1 is equal to ", reg.coef_)
-# # 8 points
+# # Complete the following print statement (replace the blanks _____ by using a command, do not hard-code the values):
+print("The linear relationship between X and y was modeled according to the equation: y = b_0 + X*b_1, \
+where the bias parameter b_0 is equal to ", reg.intercept_, " and the weight b_1 is equal to ", reg.coef_)
+# 8 points
 
-# # Predict the profit of a restaurant, if this restaurant is located in a city of 18 habitants 
-# print("the profit/loss in a city with 18 habitants is ", _________._________(_________))
-# # 8 points
+# Predict the profit of a restaurant, if this restaurant is located in a city of 18 habitants 
+print("the profit/loss in a city with 18 habitants is ", reg.predict([[18]]))
+# 8 points
     
 # """
 # PART 2: logistic regression 
@@ -61,60 +61,60 @@ plt.show()
 # The training instances consist of the two exam scores of each applicant, as well as the hiring decision.
 # """
 
-# # Open the csv file in Excel, notepad++ or any other applications to have a rough overview of the data at hand. 
+# Open the csv file in Excel, notepad++ or any other applications to have a rough overview of the data at hand. 
 
-# # Load the data from the file 'LogisticRegressionData.csv' in a pandas dataframe. Make sure all the instances 
-# # are imported properly. Name the first feature 'Score1', the second feature 'Score2', and the class 'y'
-# data = _________._________(_________, header = _________, names=['Score1', 'Score2', 'y']) # 2 points
+# Load the data from the file 'LogisticRegressionData.csv' in a pandas dataframe. Make sure all the instances 
+# are imported properly. Name the first feature 'Score1', the second feature 'Score2', and the class 'y'
+data = _________._________(_________, header = _________, names=['Score1', 'Score2', 'y']) # 2 points
 
-# # Seperate the data features (score1 and Score2) from the class attribute 
-# X = _________ # 2 points
-# y = _________ # 2 points
+# Seperate the data features (score1 and Score2) from the class attribute 
+X = _________ # 2 points
+y = _________ # 2 points
 
-# # Plot the data using a scatter plot to visualize the data. 
-# # Represent the instances with different markers of different colors based on the class labels.
-# m = ['o', 'x']
-# c = ['hotpink', '#88c999']
-# fig = plt.figure()
-# for i in range(len(data)):
-#     plt.scatter(_________['Score1'][i], _________['Score2'][i], marker=_________[data['y'][i]], color = _________[data['y'][i]]) # 2 points
-# fig.canvas.draw()
+# Plot the data using a scatter plot to visualize the data. 
+# Represent the instances with different markers of different colors based on the class labels.
+m = ['o', 'x']
+c = ['hotpink', '#88c999']
+fig = plt.figure()
+for i in range(len(data)):
+    plt.scatter(_________['Score1'][i], _________['Score2'][i], marker=_________[data['y'][i]], color = _________[data['y'][i]]) # 2 points
+fig.canvas.draw()
 
-# # Train a logistic regression classifier to predict the class labels y using the features X
-# regS = _________._________() # 2 points
-# regS._________(_________, _________) # 2 points
+# Train a logistic regression classifier to predict the class labels y using the features X
+regS = _________._________() # 2 points
+regS._________(_________, _________) # 2 points
 
-# # Now, we would like to visualize how well does the trained classifier perform on the training data
-# # Use the trained classifier on the training data to predict the class labels
-# y_pred = _________._________(_________) # 2 points
-# # To visualize the classification error on the training instances, we will plot again the data. However, this time,
-# # the markers and colors selected will be determined using the predicted class labels
-# m = ['o', 'x']
-# c = ['red', 'blue'] #this time in red and blue
-# fig = plt.figure()
-# for i in range(len(data)):
-#     plt.scatter(_________['Score1'][i], _________['Score2'][i], _________=_________[y_pred[i]], _________ = _________[y_pred[i]]) # 2 points
-# fig.canvas.draw()
-# # Notice that some of the training instances are not correctly classified. These are the training errors.
+# Now, we would like to visualize how well does the trained classifier perform on the training data
+# Use the trained classifier on the training data to predict the class labels
+y_pred = _________._________(_________) # 2 points
+# To visualize the classification error on the training instances, we will plot again the data. However, this time,
+# the markers and colors selected will be determined using the predicted class labels
+m = ['o', 'x']
+c = ['red', 'blue'] #this time in red and blue
+fig = plt.figure()
+for i in range(len(data)):
+    plt.scatter(_________['Score1'][i], _________['Score2'][i], _________=_________[y_pred[i]], _________ = _________[y_pred[i]]) # 2 points
+fig.canvas.draw()
+# Notice that some of the training instances are not correctly classified. These are the training errors.
 
-# """
-# PART 3: Multi-class classification using logistic regression 
-# Not all classification algorithms can support multi-class classification (classification tasks with more than two classes).
-# Logistic Regression was designed for binary classification.
-# One approach to alleviate this shortcoming, is to split the dataset into multiple binary classification datasets 
-# and fit a binary classification model on each. 
-# Two different examples of this approach are the One-vs-Rest and One-vs-One strategies.
-# """
+"""
+PART 3: Multi-class classification using logistic regression 
+Not all classification algorithms can support multi-class classification (classification tasks with more than two classes).
+Logistic Regression was designed for binary classification.
+One approach to alleviate this shortcoming, is to split the dataset into multiple binary classification datasets 
+and fit a binary classification model on each. 
+Two different examples of this approach are the One-vs-Rest and One-vs-One strategies.
+"""
 
-# #  One-vs-Rest method (a.k.a. One-vs-All)
+#  One-vs-Rest method (a.k.a. One-vs-All)
 
-# # Explain below how the One-vs-Rest method works for multi-class classification # 12 points
-# """
-# Your explanation goes here
-# """
+# Explain below how the One-vs-Rest method works for multi-class classification # 12 points
+"""
+Your explanation goes here
+"""
 
-# # Explain below how the One-Vs-One method works for multi-class classification # 11 points
-# """
-# Your explanation goes here
-# """
+# Explain below how the One-Vs-One method works for multi-class classification # 11 points
+"""
+Your explanation goes here
+"""
 
